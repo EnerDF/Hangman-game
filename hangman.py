e47ebaa70d4162a426e52ab683ee9d4f9293ee18
Diego Torres <diegofrancisco.torres@fisica.uaz.edu.mx>
2022-01-19 09:07:01 -0600

    Creacion del proyecto con la lista de palabras y las funciones base

---
+++ hangman.py
@@ -0,0 +1,13 @@
+def read():
+    words = []
+    with open("./data.txt", "r", unicode="utf-8") as f:
+        for line in f:
+            words.append(list)
+    print(words)
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
